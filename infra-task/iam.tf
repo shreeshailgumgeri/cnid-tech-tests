d621eacde445407182cc615fecc103e4d2f1c23b
Shreeshail G <shreeshailgumgeri@gmail.com>
2021-05-23 00:37:34 +0530

    Create iam.tf

---
+++ infra-task/iam.tf
@@ -0,0 +1,47 @@
+iam.tf
+
+resource "aws_iam_role" "machine" {
+  name = "${local.system_name}-machine-role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:*",
+                "ec2:*",
+                "elasticloadbalancing:*",
+                "autoscaling:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": [
+                        "autoscaling.amazonaws.com",
+                        "ec2scheduled.amazonaws.com",
+                        "elasticloadbalancing.amazonaws.com",
+                        "spot.amazonaws.com",
+                        "spotfleet.amazonaws.com",
+                        "transitgateway.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "machine" {
+  name_prefix = "${local.system_name}-instance"
+  role = aws_iam_role.machine.name
+}
