65cd78812c25c73028f1c1cf40300083bebd4a6a
Shreeshail G <shreeshailgumgeri@gmail.com>
2021-05-23 02:11:40 +0530

    Shreeshail : Coding Tech Task - Conde Nast : SOLVED (#1)
    * Create ec2.tf
    
    * Create iam.tf
    
    * Create input.tf
    
    * Create main.tf
    
    * Update ec2.tf
    
    * Delete guidance.md
    
    * Update iam.tf
    
    * Update main.tf
    
    * Update input.tf
    
    * Update ec2.tf
    
    * Update iam.tf
    
    * Update README.md

---
+++ infra-task/ec2.tf
@@ -0,0 +1,81 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["amazon"]
+}
+
+#resource "aws_key_pair" "test" {
+#  public_key = var.public_key
+#}
+
+
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = local.system_name
+  }
+
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+
+#  connection {
+#    type        = "ssh"
+#    host        = self.public_ip
+#    user        = "ec2-user"
+#    private_key = file(var.aws_key_pair)
+#  }
+#
+#  provisioner "remote-exec" {
+#    inline = [
+#      "sudo yum install httpd -y",
+#      "sudo service httpd start",
+#      "echo HELLO WORLD | sudo tee /var/www/html/index.html"
+#    ]
+#  }
+}
---
+++ infra-task/input.tf
@@ -0,0 +1,4 @@
+variable "public_key" {
+  description = "Public key to access the EC2 machine"
+  type = string
+}
