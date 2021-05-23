fdb13a7471bc7251ff730c9518f208ff53bef2b6
Shreeshail G <shreeshailgumgeri@gmail.com>
2021-05-23 19:05:50 +0530

    Update ec2.tf

--- infra-task/ec2.tf
+++ infra-task/ec2.tf
@@ -14,11 +14,6 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
-#resource "aws_key_pair" "test" {
-#  public_key = var.public_key
-#}
-
-
 resource "aws_security_group" "http_server_sg" {
   name = "http_server_sg"
 
@@ -63,21 +58,7 @@ resource "aws_instance" "http_server" {
   }
 
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
-  
+
   key_name = "us-east-key"
 
-#  connection {
-#    type        = "ssh"
-#    host        = self.public_ip
-#    user        = "ec2-user"
-#    private_key = file(var.aws_key_pair)
-#  }
-#
-#  provisioner "remote-exec" {
-#    inline = [
-#      "sudo yum install httpd -y",
-#      "sudo service httpd start",
-#      "echo HELLO WORLD | sudo tee /var/www/html/index.html"
-#    ]
-#  }
 }
